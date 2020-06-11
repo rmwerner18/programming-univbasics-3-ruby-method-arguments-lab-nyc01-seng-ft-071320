11a6edc3d3700437c3e9ccd375fb715e3dab6a55
Ryan Werner <rmwerner@umich.edu>
2020-06-11 19:40:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,9 +14,7 @@ def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction
-introduction_with_language
-introduction_with_language_optional
+
 
 
 
